37de4a4e6c44d5c3df865978f153235d55dee0ac
LitalHaviv <36169943+LitalHaviv@users.noreply.github.com>
2018-02-05 21:15:29 +0200

    Add files via upload
    run HtmlPage.html- the main page
    in order to look for Rss feed- put part of the title of the feed in the search box like: "Comp", "Rest", "ment"...
    json.js- is the list of all feed

---
+++ RSS1/json.js
@@ -0,0 +1,43 @@
+﻿json_arr = [
+    {
+        title: "RSS Solutions for Computer Service Companies",
+        description: "FeedForAll helps Computer Service Companies communicate with clients about cyber security and related issues.",
+        link: "http://www.feedforall.com/computer-service.html",
+        category_domain: "www.dmoz.com",
+        comments: "http://www.feedforall.com/forum",
+        pubDate: "San, 08 Dec 2010 12:00:07 -0400"
+    },
+    {
+        
+        title: "RSS Solutions for Restaurants",
+        description: "FeedForAll helps Restaurant's communicate with customers. Let your customers know the latest specials or events.",
+        link: "http://www.feedforall.com/restaurant.html",
+        category_domain: "www.dmoz.com",
+        comments: "http://www.feedforall.com/forum",
+        pubDate: "Sat, 25 May 2017 07:09:11 -0400"
+    },
+    {
+        title: "RSS Solutions for Governments",
+        description: "FeedForAll helps Governments communicate with the general public about positions on various issues, and keep the community aware of changes in important legislative issues.",
+        link: "http://www.feedforall.com/government.html",
+        category_domain: "www.dmoz.com",
+        comments: "http://www.feedforall.com/forum",
+        pubDate: "Mon, 28 Dec 2013 10:00:07 -0400"
+    },
+    {
+        title: "RSS Solutions for Realtors & Real Estate Firms",
+        description: "FeedForAll helps Realtors and Real Estate companies communicate with clients informing them of newly available properties, and open house announcements. RSS helps to reach a targeted audience and spread the word in an inexpensive, professional manner",
+        link: "http://www.feedforall.com/real-estate.html",
+        category_domain: "www.dmoz.com",
+        comments: "http://www.feedforall.com/forum",
+        pubDate: "San, 21 May 2016 08:00:07 -0400"
+    },
+    {
+        title: "RSS Solutions for Schools and Colleges",
+        description: "FeedForAll helps Educational Institutions communicate with students about school wide activities, events, and schedules.",
+        link: "http://www.feedforall.com/schools.html",
+        category_domain: "www.dmoz.com",
+        comments: "http://www.feedforall.com/forum",
+        pubDate: "Tue, 19 Oct 2004 11:09:09 -0400"
+    }
+]
---
+++ RSS1/script.js
@@ -0,0 +1,18 @@
+﻿
+
+$(document).ready(function () {
+    //callAjax();
+})
+
+var ajax_arr;
+
+function callAjax() {
+    $.ajax({
+        url: "list.json",
+        dataType: "json",
+        crossDomin: true,
+        success: function (data) {
+            ajax_arr = data;
+        }
+    });
+}
